528ffd05d538ebff5fdf3487f856adce47984758
Mohammadkaif Shaikh <66009452+iamkaif786@users.noreply.github.com>
2023-03-31 10:46:28 +0530

    Image Not Found

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -11,7 +11,7 @@ const NewsItem = (props)=> {
                         </span>
                     </div>
 
-                    <img src={!imageUrl ? "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.toptal.com%2Fmachine-learning%2Fgenerative-adversarial-networks&psig=AOvVaw1qtozV0ffcekQ-Tb53eMBo&ust=1680325686999000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNC375Czhf4CFQAAAAAdAAAAABAE" : imageUrl} className="card-img-top" alt="..." />
+                    <img src={!imageUrl ? "https://www.freeiconspng.com/thumbs/no-image-icon/no-image-icon-15.png" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
